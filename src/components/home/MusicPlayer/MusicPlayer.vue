16941cbac848a2e9fc23642898d6daf2a766bc5c
JesBrian <JesBrian666@gmail.com>
2018-04-03 23:05:33 +0800

    前台界面 - [ 播放器提高用户交互 ]

--- src/components/home/MusicPlayer/MusicPlayer.vue
+++ src/components/home/MusicPlayer/MusicPlayer.vue
@@ -1,27 +1,36 @@
 <template>
   <!-- 音乐播放器组件 -->
-  <div class="glass-bg box-show" style="width:100%; height:48px; left:0; bottom:0; position:fixed; border-radius:0; opacity:0.96; background:#282828;">
-    <audio id="homeMusicSource" @ended="changeIsPlay" controls style="top:-58px; position:absolute;">
-      <source src="../../../music/test.mp3" type="audio/mpeg" />
-    </audio>
+  <div style="width:100%; height:48px; left:0; bottom:0; position:fixed;">
+    <!-- 音乐播放器资源 -->
+    <audio id="homeMusicSource" @ended="changeIsPlay" :src="'/static/music/' + this.$store.state.musicPlayList[musicPlayNowIndex] + '.mp3'" controls style="top:-128px; position:absolute;"></audio>
 
-    <div style="width:1208px; height:88%; margin:3px auto; line-height:42px; color:#999;">
-      <!-- 播放控制 -->
-      <div style="width:98px; height:100%; display:inline-block; text-align:center;">
-        <i class="mh-if ahead" style="float:left; font-size:23px;"></i>
-        <i @click="switchMusicPlay" class="mh-if" :class="this.$store.state.musicIsPlay ? 'stop' : 'play'" style="font-size:33px;"></i>
-        <i class="mh-if back" style="float:right; font-size:23px;"></i>
-      </div>
+    <div class="glass-bg box-show" style="width:100%; height:100%; border-radius:0; opacity:0.96; background:#282828; z-index:9;">
+      <div style="width:1208px; height:88%; margin:3px auto; line-height:42px; color:#999;">
+        <!-- 播放控制 -->
+        <div style="width:98px; height:100%; display:inline-block; text-align:center;">
+          <i class="mh-if ahead" style="float:left; font-size:23px;"></i>
+          <i @click="switchMusicPlay" class="mh-if" :class="this.$store.state.musicIsPlay ? 'stop' : 'play'" style="font-size:33px;"></i>
+          <i class="mh-if back" style="float:right; font-size:23px;"></i>
+        </div>
 
-      <!-- 其他控制 -->
-      <div style="height:100%; margin-right:23px; display:inline-block; float:right;">
-        <i class="mh-if volume-on" style="margin-right:8px; font-size:23px;"></i>
-        <i @click="changeMusicPlayModel" class="mh-if" :class="this.$store.state.musicPlayModel" style="margin-right:8px; font-size:22px;"></i>
-        <i class="mh-if menu" style="margin-right:8px; position:relative; font-size:24px;">
+        <!-- 其他控制 -->
+        <div style="height:100%; margin-right:23px; display:inline-block; float:right;">
+          <i class="mh-if volume-on" style="margin-right:8px; font-size:23px;"></i>
+          <i @click="changeMusicPlayModel" class="mh-if" :class="this.$store.state.musicPlayModel" style="margin-right:8px; font-size:22px;"></i>
+          <i class="mh-if menu" style="margin-right:8px; position:relative; font-size:24px;">
           <span class="" style="width:38px; height:19px; top:3.2px; left:20px; padding:0 3px; position:absolute; z-index:-1; background:#181818; box-sizing:border-box; border:1px solid #000; border-radius:0 9px 9px 0; text-align:center; font-size:11.5px; line-height:19px; color:#666; text-shadow:2px 2px 6px #000;">
             9999
           </span>
-        </i>
+          </i>
+        </div>
+      </div>
+    </div>
+
+    <div class="glass-bg box-show" style="width:1088px; height:290px; left:50%; bottom:45px; position:absolute; transform:translate(-50%,0); z-index:-1; opacity:0.96;">
+      <div class="box-show" style="width:100%; height:38px;"></div>
+      <div style="width:100%; height:243px; margin:3px 0;">
+        <div class="box-show" style="width:62%; height:100%; margin-left:3px; float:left; display:inline-block;"></div>
+        <div class="box-show" style="width:37.3%; height:100%; margin-right:3px; float:right; display:inline-block;"></div>
       </div>
     </div>
   </div>
@@ -41,6 +50,20 @@ export default {
     this.musicSource = document.getElementById('homeMusicSource')
   },
 
+  watch: {
+    musicPlayNowIndex () {
+      setTimeout(() => {
+        this.musicSource.play()
+      }, 68)
+    }
+  },
+
+  computed: {
+    musicPlayNowIndex () {
+      return this.$store.state.musicPlayNowIndex
+    }
+  },
+
   methods: {
     switchMusicPlay () {
       this.$store.commit('changeMusicIsPlayStatus')
@@ -48,21 +71,29 @@ export default {
     },
 
     changeIsPlay () {
-      if (this.$store.state.musicPlayModel === 'single-loop') {
+      if (this.$store.state.musicPlayModel === 'loop') {
+        this.$store.commit('changeMusicPlayNowIndex')
+      } else if (this.$store.state.musicPlayModel === 'single-loop') {
         this.musicSource.play()
+      } else {
+        let indexTemp
+        do {
+          indexTemp = Number.parseInt(this.$store.state.musicPlayList.length * Math.random())
+        } while (indexTemp === this.$store.state.musicPlayNowIndex)
+        this.$store.commit('changeMusicPlayNowIndex', indexTemp)
       }
     },
 
     changeMusicPlayModel () {
-      let tyep
+      let type
       if (this.$store.state.musicPlayModel === 'loop') {
-        tyep = 'single-loop'
+        type = 'single-loop'
       } else if (this.$store.state.musicPlayModel === 'single-loop') {
-        tyep = 'random'
+        type = 'random'
       } else {
-        tyep = 'loop'
+        type = 'loop'
       }
-      this.$store.commit('changeMusicPlayModel', tyep)
+      this.$store.commit('changeMusicPlayModel', type)
     }
   }
 }
--- src/config/vuex/store.js
+++ src/config/vuex/store.js
@@ -6,7 +6,9 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     musicIsPlay: false,
-    musicPlayModel: 'loop' // 三种播放模式 [ loop-歌单循环，single-loop-单曲循环，random-歌单里歌曲随机播放 ]
+    musicPlayModel: 'loop', // 三种播放模式 [ loop-歌单循环，single-loop-单曲循环，random-歌单里歌曲随机播放 ]
+    musicPlayNowIndex: 0,
+    musicPlayList: ['test1', 'test2', 'test3']
   },
   mutations: {
     /**
@@ -24,6 +26,24 @@ export default new Vuex.Store({
      */
     changeMusicPlayModel (state, type = 'loop') {
       state.musicPlayModel = type
+    },
+
+    /**
+     * 改变现在播放音乐在播放列表的索引号
+     * @param state
+     * @param nowIndexNum
+     * @return {boolean}
+     */
+    changeMusicPlayNowIndex (state, nowIndexNum = -1) {
+      if (nowIndexNum === -1) {
+        if ((state.musicPlayNowIndex + 1) === state.musicPlayList.length) {
+          state.musicPlayNowIndex = 0
+        } else {
+          state.musicPlayNowIndex++
+        }
+      } else {
+        state.musicPlayNowIndex = nowIndexNum
+      }
     }
   }
 })
