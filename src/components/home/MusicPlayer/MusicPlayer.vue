98f3cda89a4961af611c7cd7ddae9b8b213a519c
JesBrian <JesBrian666@gmail.com>
2018-04-04 11:30:47 +0800

    前台界面 - [ 播放器提高用户交互 again & again ]

--- src/components/home/MusicPlayer/MusicPlayer.vue
+++ src/components/home/MusicPlayer/MusicPlayer.vue
@@ -2,15 +2,31 @@
   <!-- 音乐播放器组件 -->
   <div style="width:100%; height:48px; left:0; bottom:0; position:fixed;">
     <!-- 音乐播放器资源 -->
-    <audio id="homeMusicSource" @ended="changeIsPlay" :src="'/static/music/' + this.$store.state.musicPlayList[musicPlayListNowIndex] + '.mp3'" controls style="top:0px; position:absolute;"></audio>
+    <audio id="homeMusicSource" @ended="changeIsPlay" :src="'/static/music/' + this.$store.state.musicPlayList[musicPlayListNowIndex] + '.mp3'" style="top:0px; position:absolute;"></audio>
 
+    <!-- 播放器主控制 -->
     <div class="glass-bg box-show" style="width:100%; height:100%; border-radius:0; opacity:0.96; background:#181818; z-index:9;">
       <div style="width:1208px; height:88%; margin:3px auto; line-height:42px; color:#999;">
         <!-- 播放控制 -->
-        <div style="width:98px; height:100%; display:inline-block; text-align:center;">
+        <div style="width:98px; height:100%; float:left; display:inline-block; text-align:center;">
           <i class="mh-if ahead" style="float:left; font-size:23px;"></i>
           <i @click="switchMusicPlay" class="mh-if" :class="this.$store.state.musicIsPlay ? 'stop' : 'play'" style="font-size:33px;"></i>
           <i class="mh-if back" style="float:right; font-size:23px;"></i>
+        </div>
+        <!-- 歌曲控制 -->
+        <div style="width:968px; height:100%; padding:0 16px 0 28px; box-sizing:border-box; display:inline-block; border-right:2px solid lightcoral;">
+          <router-link to="/song" class="glass-bg" style="width:31px; height:31px; margin:4px 0; float:left; display:inline-block; box-shadow:0 0.5px 3px -2px #FFF;">
+            <img v-lazy="require('../../../../static/img/default/default_album.jpg')" style="width:100%; height:100%; border-radius:3.5px;" />
+          </router-link>
+          <div style="width:768px; height:100%; margin-left:28px; display:inline-block;">
+            <div style="width:100%; height:50%; margin:0; display:block; background:lightpink"></div>
+            <div style="width:80%; height:50%; float:left; background:lightgreen"></div>
+            <div style="width:20%; height:50%; float:right; background:#3fffbc"></div>
+          </div>
+          <div style="float:right;">
+            <i class="mh-if add-collection" style="margin-right:6px; font-size:24px;"></i>
+            <i class="mh-if share" style="margin-right:6px; font-size:22px; font-weight:700;"></i>
+          </div>
         </div>
         <!-- 其他控制 -->
         <div style="height:100%; margin-right:23px; display:inline-block; float:right;">
@@ -18,17 +34,18 @@
           <i @click="changeMusicPlayModel" class="mh-if" :class="this.$store.state.musicPlayModel" style="margin-right:8px; font-size:22px;"></i>
           <i @click="changeMusicPlayListContentShowStatus" class="mh-if menu" style="margin-right:8px; position:relative; z-index:2; font-size:24px;">
             <span style="width:38px; height:19px; top:3.2px; left:20px; padding:0 3px; position:absolute; z-index:-1; background:#181818; box-sizing:border-box; border:1px solid #000; border-radius:0 9px 9px 0; text-align:center; font-size:11.5px; line-height:19px; color:#666; text-shadow:2px 2px 6px #000;">
-              9999
+              {{ this.$store.state.musicPlayList.length }}
             </span>
           </i>
         </div>
       </div>
     </div>
 
+    <!-- 播放列表内容区域 -->
     <div v-if="this.$store.state.musicPlayListContentShowStatus === true" class="glass-bg box-show" style="width:1028px; height:290px; left:50%; bottom:45px; position:absolute; transform:translate(-50%,0); z-index:-1; background:#181818; opacity:0.96; border-radius:8px 8px 0 0;">
       <div class="box-show" style="width:100%; height:38px; border-radius:8px 8px 0 0; line-height:40px;">
         <div style="width:62%; height:100%; padding:0 2%; float:left; box-sizing:border-box; color:#DDD;">
-          <span>播放列表 [ 9999 ]</span>
+          <span style="font-weight:700;">播放列表 [ {{ this.$store.state.musicPlayList.length }} ]</span>
           <div style="float:right;">
             <a style="font-size:14px;"><i class="mh-if collection-music" style="margin:0 3px 0 18px;"></i>收藏全部</a>
             <a style="font-size:14px;"><i class="mh-if trash-2" style="margin:0 3px 0 18px;"></i>清空列表</a>
@@ -51,7 +68,46 @@
           </gemini-scrollbar>
         </div>
         <!-- 歌词滚动区域 -->
-        <div style="width:38%; height:100%; float:right; display:inline-block; box-shadow:inset 0 -2px 1px -1px rgba(0, 0, 0, 0.2), 0 12px 12px rgba(0, 0, 0, 0.5), 0 4px 6px rgba(0, 0, 0, 0.3), inset 0 0 0 1px #272727;"></div>
+        <div style="width:38%; height:100%; float:right; display:inline-block; position:relative; box-shadow:inset 0 -2px 1px -1px rgba(0, 0, 0, 0.2), 0 12px 12px rgba(0, 0, 0, 0.5), 0 4px 6px rgba(0, 0, 0, 0.3), inset 0 0 0 1px #272727;">
+          <i class="mh-if question" style="right:18px; top:53px; position:fixed; font-size:25px; color:#888;"></i>
+          <div style="width:100%; height:100%; padding:12px 0 3px; box-sizing:border-box; text-align:center;">
+            <gemini-scrollbar>
+              <ul>
+                <li>rdtGbuybjk</li>
+                <li>rbuyb</li>
+                <li>rdt非常需要他vsvdGbuybjk</li>
+                <li>vvsdv</li>
+                <li>rdt56bjk</li>
+                <li>rbuyb</li>
+                <li>rdtvGy尝试</li>
+                <li>vvsdv</li>
+                <li>rdtGbuybjk</li>
+                <li>rbuyb</li>
+                <li>rdt非常需要buybjk</li>
+                <li>vvsdv</li>
+                <li>r产生dt56bjk</li>
+                <li>rbuyb</li>
+                <li>rdtvGy尝试</li>
+                <li>vvscasdv</li>
+                <li>rbuyb</li>
+                <li>rdt非常需要他vsvdGbuybjk</li>
+                <li>vvsdv</li>
+                <li>rdt56bjk</li>
+                <li>rbuyb</li>
+                <li>rdtvGy尝试</li>
+                <li>vvsdv</li>
+                <li>rdtGbuybjk</li>
+                <li>rbuyb</li>
+                <li>rdt非常需要buybjk</li>
+                <li>vvsdv</li>
+                <li>r产生dt56bjk</li>
+                <li>rbuyb</li>
+                <li>rdtvGy尝试</li>
+                <li>vvscasdv</li>
+              </ul>
+            </gemini-scrollbar>
+          </div>
+        </div>
       </div>
     </div>
 
