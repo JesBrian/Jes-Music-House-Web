eabd36a7d4b8604d81fa585927a7f1a90f752ff1
JesBrian <JesBrian666@gmail.com>
2018-04-06 10:02:48 +0800

    前台界面 - [ 播放器美化歌词报错 ]

--- src/components/home/MusicPlayer/MusicPlayer.vue
+++ src/components/home/MusicPlayer/MusicPlayer.vue
@@ -92,7 +92,14 @@
         </div>
         <!-- 歌词滚动区域 -->
         <div style="width:38%; height:100%; float:right; display:inline-block; position:relative; box-shadow:inset 0 -2px 1px -1px rgba(0, 0, 0, 0.2), 0 12px 12px rgba(0, 0, 0, 0.5), 0 4px 6px rgba(0, 0, 0, 0.3), inset 0 0 0 1px #272727;">
-          <i class="mh-if question" style="right:18px; top:53px; position:fixed; font-size:25px; opacity:0.38; z-index:9;"></i>
+          <!-- 歌词报错 -->
+          <i class="mh-if question">
+            <ul class="glass-bg box-show">
+              <li class="box-shadow"><router-link to="/">上传歌词</router-link></li>
+              <li class="box-shadow"><router-link to="/">歌词报错</router-link></li>
+            </ul>
+          </i>
+          <!-- 滚动歌词区域 -->
           <div style="width:100%; height:100%; padding:12px 0 3px; box-sizing:border-box; text-align:center; line-height:1.5em;">
             <gemini-scrollbar>
               <ul>
@@ -271,6 +278,24 @@ export default {
     display:block;
   }
   #volumeButton:hover > #volumeBar:hover {
+    display:block;
+  }
+
+  .question {
+    right:38px; top:53px; position:fixed; font-size:25px; opacity:0.38; z-index:9;
+  }
+  .question a {
+    color:#999;
+    text-shadow:none;
+  }
+  .question > ul.glass-bg{
+    width:76px; top:25px;  left:50%; padding:3px; position:absolute; transform:translate(-50%, 0); font-size:14px; text-align:center;
+    display:none;
+  }
+  .question > ul.glass-bg > li{
+    padding:6px 0;
+  }
+  .question:hover > ul.glass-bg{
     display:block;
   }
 </style>
