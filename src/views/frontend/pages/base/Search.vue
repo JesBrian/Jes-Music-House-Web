9ecd4b7609be1974bdf492bddccc871e6abd9941
JesBrian <jesbrian666@gmail.com>
2018-07-02 16:31:16 +0800

    搜索页面 - [ 完善交互 ]

--- src/views/frontend/pages/base/Search.vue
+++ src/views/frontend/pages/base/Search.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="homeLayout">
 
-    <div style="width:88%; height:38px; margin:0 auto 10px; padding-top:18px; box-shadow:0 5px 8px -5px #00d8ff; text-align:center; border-bottom:1px solid #00d8ff; line-height:27px; font-weight:700;">
+    <div style="width:88%; height:38px; margin:0 auto 10px; padding-top:28px; box-shadow:0 4px 3px -3px #00d8ff; text-align:center; border-bottom:1px solid #00d8ff; line-height:27px; font-weight:700;">
       <div @click="changeSearchType('song')" :class="{'active' : searchType === 'song'}" class="super-btn-out">
         <span class="super-btn-in mh-if music-list"> 单曲</span>
       </div>
@@ -14,11 +14,16 @@
       <div @click="changeSearchType('user')" :class="{'active' : searchType === 'user'}" class="super-btn-out">
         <span class="super-btn-in mh-if datum"> 用户</span>
       </div>
+
+      <label class="super-btn-out active" style="width:268px; height:32px; margin:-6px 28px 0; float:right; position:relative; border:none; border-radius:16px;">
+        <input v-model="searchKey" type="text" class="super-btn-in" placeholder="搜索本歌单音乐" style="width:258px; height:76%; top:50%; padding:0 12px; box-sizing:border-box; border-radius:12px; text-align:left; font-size:17px;"/>
+        <i @click="goSearch" class="mh-if search" style="top:2px; right:14px; position:absolute; z-index:5;"></i>
+      </label>
     </div>
 
     <div style="width:88%; margin:0 auto; text-align:right;">
       <span style="margin-right:38px; color:#CCC; font-size:14px;">
-        搜索 " <span style="color:#00d8ff; text-decoration:underline;">心碎</span> "，共找到 <span style="color:#00d8ff;">2222</span> 首单曲
+        搜索 " <span style="color:#00d8ff; text-decoration:underline;">{{ searchKey }}</span> "，共找到 <span style="color:#00d8ff;">2222</span> 首单曲
       </span>
       <div style="width:100%; margin-top:6px;">
         <ul>
@@ -44,13 +49,28 @@ export default {
 
   data () {
     return {
-      searchType: 'song'
+      searchType: 'song',
+      searchKey: this.$route.params['searchKey']
+    }
+  },
+
+  watch: {
+    searchType () {
+      this.$router.push(`/search/${this.searchType}/${this.searchKey}`)
     }
   },
 
   methods: {
     changeSearchType (type = 'song') {
       this.searchType = type
+    },
+
+    goSearch () {
+      if ((this.searchKey === this.$route.params['searchKey']) || (this.searchKey === '')) {
+        return false
+      }
+
+      this.$router.push(`/search/${this.searchType}/${this.searchKey}`)
     }
   }
 }
