0eaf28fbdb33f60d1e38df51a5e9768f796b33ef
JesBrian <jesbrian666@gmail.com>
2018-09-18 11:14:48 +0800

    组件 - [ loading & switch ]

---
+++ src/components/common/loading/PageLoading.vue
@@ -0,0 +1,53 @@
+<template>
+  <!-- 加载等待loading组件 -->
+  <div class="loader">
+    <div class="loading-1"></div>
+    <div class="loading-2">Loading ...</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageLoading'
+}
+</script>
+
+<style scoped>
+  .loader {
+    width: 208px; top:48%; left:50%; position:absolute; transform:translate(-50%,-50%);
+  }
+
+  .loader .loading-1 {
+    width:100%; height:10px; position:relative;
+    border:1px solid #2893ff; border-radius:10px;
+    animation:turn 4s linear 1.75s infinite;
+  }
+
+  .loader .loading-1:before {
+    content:""; width:0; height:100%; position:absolute; display:block;
+    border-radius:10px; background:linear-gradient(to right, #64E0FF, #2893ff); box-shadow:3px 0 10px 0 #69d2e7;
+    animation:load 2s linear infinite;
+  }
+
+  .loader .loading-2 {
+    width:100%; top:10px; margin-top:8px; position:absolute;
+    color:#2dbeff; font-size:22px; text-align:center;
+    animation: bounce 2s linear infinite;
+  }
+
+  @keyframes load {
+    0% { width:0; }
+    87.5%, 100% { width:100%; }
+  }
+
+  @keyframes turn {
+    0% { transform:rotateY(0deg); }
+    6.25%, 50% { transform:rotateY(180deg); }
+    56.25%, 100% { transform:rotateY(360deg); }
+  }
+
+  @keyframes bounce {
+    0%, 100% { top:10px; }
+    12.5% { top:30px; }
+  }
+</style>
---
+++ src/components/common/switch/SuperSwitch.vue
@@ -0,0 +1,134 @@
+<template>
+  <label :class="size">
+    <input v-model="switchState" @change="switchChangeEvent" :class="['mui-switch', shape]" :data-open="switchOpen" :data-close="switchClose" type="checkbox">
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'SuperSwitch',
+
+  props: {
+    size: {
+      type: String,
+      default: ''
+    },
+    shape: {
+      type: String,
+      default: 'square'
+    },
+    switchState: {
+      type: Boolean,
+      default: false
+    },
+    switchOpen: {
+      type: String,
+      default: '开'
+    },
+    switchClose: {
+      type: String,
+      default: '关'
+    }
+  },
+
+  model: {
+    prop: 'switchState', // 父组件 v-model 传的值, props 需有同名
+    event: 'switchChange' // 修改父组件的值事件, ps：$emit('事件名', 要修改父组件 v-model 的值)
+  },
+
+  data: function () {
+    return {}
+  },
+
+  computed: {},
+
+  methods: {
+    switchChangeEvent: function () {
+      this.$emit('switchChange', this.switchState)
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+  input {
+    outline: none!important;
+    outline-width: 0;
+  }
+  label {
+    display: inline-block;
+    position: relative;
+  }
+  label.large {
+    transform: scale(1.2, 1.2);
+  }
+  label.small {
+    transform: scale(0.8, 0.8);
+  }
+  .mui-switch {
+    width: 58px;
+    height: 28px;
+    position: relative;
+    display: inline-block;
+    box-sizing: border-box;
+    border: 1px solid #dfdfdf;
+    background-color: #fdfdfd;
+    box-shadow: #dfdfdf 0 0 0 0 inset;
+    background-clip: content-box;
+    -webkit-appearance: none;
+    user-select: none;
+    outline: none;
+    transition: border cubic-bezier(0, 0, 0, 1) 0.4s, box-shadow cubic-bezier(0, 0, 0, 1) 0.4s;
+  }
+  .mui-switch.circle {
+    border-radius: 20px;
+  }
+  .mui-switch.square {
+    border-radius: 4px;
+  }
+  .mui-switch::before {
+    content: '';
+    width: 24px;
+    height: 24px;
+    top: 0.6px;
+    left: 0;
+    position: absolute;
+    background: #FFF;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.4);
+    transition: left 0.28s;
+    z-index: 9;
+  }
+  .mui-switch.circle::before {
+    border-radius: 50%;
+  }
+  .mui-switch.square::before {
+    border-radius: 3px;
+  }
+  .mui-switch::after {
+    content: attr(data-close);
+    width: 32px;
+    height: 26px;
+    top: 0;
+    right: 0;
+    text-align: center;
+    position: absolute;
+    word-break: break-all;
+    line-height: 26px;
+    overflow: hidden;
+  }
+  .mui-switch:checked::after {
+    content: attr(data-open);
+    left: 0;
+  }
+  .mui-switch:checked {
+    border-color: #64bd63;
+    box-shadow: #64bd63 0 0 0 16px inset;
+    background-color: #64bd63;
+    transition: border ease 0.4s, box-shadow ease 0.4s, background-color ease 1.2s;
+  }
+  .mui-switch:checked::before {
+    left: 31px;
+  }
+
+</style>
