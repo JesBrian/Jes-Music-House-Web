630de57bc86f74b1de1b0aabd0f0dcb601989b6e
JesBrian <jesbrian666@gmail.com>
2018-10-22 08:23:16 +0800

    message - [ improve category ]

--- src/views/frontend/pages/music/Song.vue
+++ src/views/frontend/pages/music/Song.vue
@@ -12,7 +12,7 @@
         <div v-for="n in 5" to="/song" :key="`${n}2288`" class="box-shadow" style="height:42px; display:flex;">
           <div style="flex:1; display:flex; flex-direction:column; justify-content:center;">
             <router-link to="/song" style="color:#CCC; font-size:15px;">Song Name</router-link>
-            <router-link to="/singer-detail/hot-list/1" style="margin-top:4px; color:#888; font-size:13px;">Singer 名称 Name</router-link>
+            <router-link to="/singer-detail/hot-song/1" style="margin-top:4px; color:#888; font-size:13px;">Singer 名称 Name</router-link>
           </div>
           <div style="width:48px; margin-right:5px; display:flex; justify-content:space-around; align-items:center;">
             <i class="mh-if play"></i>
--- src/views/frontend/pages/user/Message.vue
+++ src/views/frontend/pages/user/Message.vue
@@ -2,6 +2,16 @@
   <div id="homeLayout">
 
     <div class="user-message-category">
+      <dl>
+        <dt class="user-message-category-title box-shadow">我的消息</dt>
+
+        <dd v-for="(item, index) in messageCategory" :key="`category${index}`" class="box-shadow">
+          <div @click="changeMessageType(item.id)" :class="{active: type === item.id}" class="message-category-cell">
+            <i :class="item.logo" class="message-category-cell-logo mh-if"></i>
+            <span class="message-category-cell-label">{{ item.name }}</span>
+          </div>
+        </dd>
+      </dl>
     </div>
 
     <div class="user-message-container box-show">
@@ -12,7 +22,25 @@
 
 <script>
 export default {
-  name: 'Message'
+  name: 'Message',
+
+  data () {
+    return {
+      type: 'comment',
+
+      messageCategory: [
+        {id: 'comment', logo: 'feedback', name: '评论'},
+        {id: 'feedback', logo: 'mail', name: '私信'},
+        {id: 'notify', logo: 'horn', name: '通知'}
+      ]
+    }
+  },
+
+  methods: {
+    changeMessageType (type = 'comment') {
+      this.type = type
+    }
+  }
 }
 </script>
 
@@ -23,6 +51,31 @@ export default {
 
   .user-message-category {
     width:20%; height:100%;
+
+    &-title {
+      width:100%; height:68px; font-size:22px; font-weight:700; line-height:75px; text-indent:1em;
+    }
+
+    .message-category-cell {
+      width:100%; height:53px; display:flex; flex-direction:row; align-items:center;
+
+      &-logo {
+        margin:0 10px 0 18px; font-size:23px;
+      }
+      &-label {
+        color:#AAA;
+      }
+
+      &:hover > .message-category-cell-label {
+        color:#EEE;
+      }
+      &.active {
+        > .message-category-cell-label {
+          color:#00d8ff;
+        }
+        background:#1D1D1D;
+      }
+    }
   }
 
   .user-message-container {
